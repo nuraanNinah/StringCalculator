b23e2e34fb2aec7723892690c50813adcb0f9d59
nuraanNinah <60215982+nuraanNinah@users.noreply.github.com>
2020-05-04 03:45:05 +0200

    Add files via upload

---
+++ string_calculator_spec.js
@@ -0,0 +1,75 @@
+let myAdd = require("../src/string_calculator");
+let add = myAdd.add
+
+describe("add", function () {
+    it(`should return 0`, function () {
+        expect(add("")).toBe(0);
+    });
+    it(`should return 1`, function () {
+        expect(add("1")).toBe(1);
+    });
+    it(`should return 2`, function () {
+        expect(add("1,1")).toBe(2);
+    });
+    it(`should return 10`, function () {
+        expect(add("1,2,3,4")).toBe(10);
+    });
+    it(`should return 6`, function () {
+        expect(add("1\n2,3")).toBe(6);
+    });
+    it(`should return 3`, function () {
+        expect(add("//;\n1;2")).toBe(3);
+    });
+    it(`should return 3`, function () {
+        expect(add("//4\n142")).toBe(3);
+    });
+    it(`should return  Negatives not allowed`, function () {
+        expect(function () {
+            add("-1,2,3,4")
+        }).toThrow("negatives not allowed -1")
+    });
+    it(`should return 3`, function () {
+        expect(add("//;\n1000;1;2")).toBe(3);
+    });
+    it(`should return 3`, function () {
+        expect(add("//***\n1***2***3")).toBe(6);
+    });
+
+    it(`should throw error`, function () {
+        expect(add("-1,-2,3,4")).toThrow("negatives not allowed -1,-2");
+    });
+
+    it(`should return 3`, function () {
+        expect(add("//;\n1000;1;2")).toBe(3);
+    });
+
+    it(`should return 6`, function () {
+        expect(add("//***\n1***2***3")).toBe(6);
+    });
+
+    it(`should return 6`, function () {
+        expect(add("//[:D][%]\n1:D2%3")).toBe(6);
+    });
+
+    it(`should return 6`, function () {
+        expect(add("[***][%%%]\n1***2%%%3")).toBe(6);
+    });
+
+    it(`should return 6`, function () {
+        expect(add("//[(-_-')][%]\n1(-_-')2%3")).toBe(6);
+    });
+
+    it(`should throw error 2`, function () {
+        expect(add("//;\n1000;1;2;")).toThrow("Invalid input");
+    });
+
+    it(`should throw error 3`, function () {
+        expect(add("   //;\n1000,1;2")).toThrow("Invalid input");
+    });
+
+    it(`should throw error 4`, function () {
+        expect(add("1,2,3//;\n1000,1;2")).toThrow("Invalid input");
+    });
+
+
+});
